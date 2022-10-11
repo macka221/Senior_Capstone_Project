0b56f2ad646a5534a2592055caa6155b1af2ae72
zach <zachare18@gmail.com>
2022-10-11 00:55:41 -0400

    adding a unique id feature and linking first set of endpoints

--- src/app/api/downstreamServices/downstreamService.py
+++ src/app/api/downstreamServices/downstreamService.py
@@ -1,14 +1,17 @@
 from app.api.api_services import institution
 
 
-class businessServices(object):
-    def userCreation(institution, name, email, manager):
+class businessServices:
+    def userCreation(self, institution, name, email, manager):
         return True
 
-    def institutionCreation(name, address, campuses):
+    def institutionCreation(self, name, address, campuses):
+        return institution.createNewInstitution(name=name, address=address, campus=campuses)
+
+    def buildingCreation(self, instituteId, name, address, rooms, manager, provider, consumption, cost):
         return True
 
-    def buildingCreation(instituteId, name, address, rooms, manager, provider, consumption, cost):
-        return True
+    def getInstitution(self, institute_id):
+        return institution.getInstitute_from_Institutes(institute_id=institute_id)
 
 
