98630a33975929066b997124ff812eab43210e98
justinwynn719 <justinwynn719@gmail.com>
2022-11-14 20:11:34 -0500

    fixed return errors on select statements

--- src/app/api/downstreamServices/database/Query.py
+++ src/app/api/downstreamServices/database/Query.py
@@ -2,23 +2,23 @@ from Creation_Functions import *
 
 get_connection()
 
-#create_institution("MO", "Morehouse_College", "GP")
-#create_institution("EM", "Emory", "GP")
-#create_institution("SP", "Spelman", "GP")
+create_institution("MO", "Morehouse_College", "GP")
+create_institution("EM", "Emory", "GP")
+create_institution("SP", "Spelman", "GP")
 
-#create_campus("MO", "MO-MA", "GP", 45.77, 63.44)
-#create_campus("MO", "MO-ME", "GP", 48.19, 64.37)
+create_campus("MO", "MO-MA", "GP", 45.77, 63.44)
+create_campus("MO", "MO-ME", "GP", 48.19, 64.37)
 
-#create_campus("EM", "EM-MA", "GP", 42.22, 69.45)
-#create_campus("SP", "SP-MA", "GP", 43.13, 63.95)
+create_campus("EM", "EM-MA", "GP", 42.22, 69.45)
+create_campus("SP", "SP-MA", "GP", 43.13, 63.95)
 
-#create_building("MO-MA", "MO-MA-DANS", "Dansby", "830 Westview Dr")
-#create_building("EM-MA", "EM-MA-BUI", "Building", "address")
-#create_building("SP-MA", "SP-MA-COS", "Cosby", "address")
+create_building("MO-MA", "MO-MA-DANS", "Dansby", "830 Westview Dr")
+create_building("EM-MA", "EM-MA-BUI", "Building", "address")
+create_building("SP-MA", "SP-MA-COS", "Cosby", "address")
 
-#create_room("MO-MA-DANS", "206", "MO-MA-DANS-206", 10, 12, 10, 120)
+create_room("MO-MA-DANS", "206", "MO-MA-DANS-206", 10, 12, 10, 120)
 
-#create_user("MO748be", "MO", "Justin", "Wynn", "justinwynn719@gmail.com", False, True, 1234)
+create_user("MO748be", "MO", "Justin", "Wynn", "justinwynn719@gmail.com", False, True, 1234)
 
 getInstCamps("MO")
 getInstCamps("EM")
